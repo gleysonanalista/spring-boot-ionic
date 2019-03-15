db74031f255a72d9939a0703c98b8f20175d3889
Gleyson Costa <gleysonanalista@gmail.com>
2019-03-14 21:06:45 -0300

    Criaçao Class Dominio

--- src/main/java/com/gleyson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gleyson/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.gleyson.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gleyson.cursomc.dominio.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está no controle de Tudo";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Gleyson Costa");
+		Categoria cat2 = new Categoria(2,"Bruno Costa");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
