6fcc2211b11c616de1d70563952aa168c415c92b
Gleyson Costa <gleysonanalista@gmail.com>
2019-03-19 18:05:46 -0300

    Instanciação de Serviços

--- src/main/java/com/gleyson/cursomc/CursomcApplication.java
+++ src/main/java/com/gleyson/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.gleyson.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.gleyson.cursomc.dominio.Categoria;
+import com.gleyson.cursomc.repository.CategoriaRepositorio;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepositorio cat ;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria dado1 = new Categoria(1, "Casa da moeda");
+		Categoria dado2 = new Categoria(null, "Viva a Vida");
+		
+		cat.save(Arrays.asList(dado1, dado2));
+		
 	}
 
 }
