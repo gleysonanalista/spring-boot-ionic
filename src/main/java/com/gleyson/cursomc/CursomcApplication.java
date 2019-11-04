6cc29d51a409ebf28cbcba7be3f4d2da14246b24
gleyson costa <gleysonanalista@gmail.com>
2019-11-04 20:25:10 -0300

    Inclusão de novos produtos e categorias

--- src/main/java/com/gleyson/cursomc/CursomcApplication.java
+++ src/main/java/com/gleyson/cursomc/CursomcApplication.java
@@ -81,16 +81,39 @@ public class CursomcApplication implements CommandLineRunner{
 		Produto p1 = new Produto(null, "PS4", 2.500);
 		Produto p2 = new Produto(null, "XBOX ONE", 2.000);
 		Produto p3 = new Produto(null, "NINTENDO SWITCH", 2.000);
-		
+		Produto p4 = new Produto(null, "SEGA SATURNO", 1.000);
+		Produto p5 = new Produto(null, "SUPER NINTENDO", 800.00);
+		Produto p6= new Produto(null, "NEO GEO", 500.00);
+		Produto p7 = new Produto(null, "MEGA DRIVE", 400.00);
+		Produto p8 = new Produto(null, "GAME BOY", 500.00);
+		Produto p9 = new Produto(null, "NINTENDO 64", 300.00);
+		Produto p10 = new Produto(null, "ATARI", 600.00);
+		Produto p11 = new Produto(null, "XBOX 360", 500.00);
+		Produto p12 = new Produto(null, "PS3", 500.00);
+
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat2.getProdutos().addAll(Arrays.asList(p2,p4));
+		cat3.getProdutos().addAll(Arrays.asList(p5,p6));
+		cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
+		cat5.getProdutos().addAll(Arrays.asList(p8));
+		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
+		cat7.getProdutos().addAll(Arrays.asList(p11, p12));
 		
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
+		p2.getCategorias().addAll(Arrays.asList(cat2,cat4));
+		p3.getCategorias().addAll(Arrays.asList(cat7));
+		p4.getCategorias().addAll(Arrays.asList(cat2));
+		p5.getCategorias().addAll(Arrays.asList(cat3));
+		p6.getCategorias().addAll(Arrays.asList(cat3));
+		p7.getCategorias().addAll(Arrays.asList(cat4));
+		p8.getCategorias().addAll(Arrays.asList(cat5));
+		p9.getCategorias().addAll(Arrays.asList(cat6));
+		p10.getCategorias().addAll(Arrays.asList(cat6));
+		p11.getCategorias().addAll(Arrays.asList(cat7));
+		p12.getCategorias().addAll(Arrays.asList(cat7));
 		
 		cat.save(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
-		prod.save(Arrays.asList(p1, p2, p3));
+		prod.save(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12));
 		
 		Estado estado1 = new Estado(null, "Minas Gerais");
 		Estado estado2 = new Estado(null, "Bahia");
